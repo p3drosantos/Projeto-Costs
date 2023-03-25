ffb40b213bb1c82a4c4ad80edcc70cfaec5a361b
Pedro Santos <P3dro.santosc@gmail.com>
2023-03-25 11:30:07 -0300

    implatação de busca de dados na api e sistema de loading

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,18 @@
 import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 import Message from "../layout/Message"
+import Container from '../layout/Container'
+import Loading from "../layout/Loading"
+import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
+
+import styles from './Projects.module.css'
+
 
 function Projects(){
+    const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
 const location = useLocation()
 let message = ''
@@ -10,10 +20,46 @@ if(location.state){
     message = location.state.message
 }
 
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers:{
+                    'Content-type': 'application/json',
+                },
+              }).then(resp => resp.json())
+              .then(data => {
+                    setProjects(data)
+                    setRemoveLoading(true)
+              })
+              .catch(err => console.log(err))            
+        }, 250);
+
+    }, [])
+
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newprojects" text="Criar Projeto"/>
+            </div>
             {message && <Message type="success" msg={message}/>}
+            <Container customClass="start">
+                {projects.length > 0 &&
+                projects.map((project) => (
+                    <ProjectCard
+                    id={project.id}
+                    name={project.name}
+                    budget={project.budget}
+                    category={project.category.name}
+                    key={project.id}
+                     />
+                ))}
+                {!removeLoading && <Loading/>}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não Há Projetos Cadastrados!</p>
+                )} 
+            </Container>
         </div>
     )
 }
